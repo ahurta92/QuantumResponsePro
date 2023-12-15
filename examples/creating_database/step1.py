b1637c9d485a277595fd85054c7b6a17d5ff95c7
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2023-12-15 13:23:48 -0500

    Illustrating how to create a database

--- examples/creating_database/step1.py
+++ examples/creating_database/step1.py
@@ -3,13 +3,14 @@ from quantumresponsepro import DatabaseGenerator
 
 # Step 1 is to generate the excited state data for the molecules in the database
 
-dir = "/mnt/data/madness_data/nacl_ioniazation"
+database_path = Path("/mnt/data/madness_data/database_director")
 
-db_gen = DatabaseGenerator(Path(dir))
-
+db_gen = DatabaseGenerator(database_path)
 xc = "hf"
 basis = 'aug-cc-pVTZ'
-
 db_gen.get_dalton_excited_json(xc, [basis], True)
+# looks for molecules in the database directory and generates the excited-state data from which to
+# generate frequency.json input file.
+
 
 # freq_json = dg.get_frequency_json(9, xc, op, basis, .5)
--- examples/creating_database/step2.py
+++ examples/creating_database/step2.py
@@ -1,10 +1,31 @@
 from pathlib import Path
-from quantumresponsepro import DatabaseGenerator
+from quantumresponsepro import DatabaseGenerator, DaltonRunner
 
-database_path = Path('/mnt/data/madness_data/nacl_ioniazation')
+database_path = Path('/mnt/data/madness_data/database')
 basis = 'aug-cc-pVTZ'
 xc = 'hf'
 op = 'dipole'
 
+# the basis set determines frequency of the excited state
 dg = DatabaseGenerator(database_path)
 freq_json = dg.get_frequency_json(9, xc, op, basis, .5)
+# creating frequency.json file
+
+dr = DaltonRunner(database_path, True)
+
+single=['aug-cc-p{zeta}Z'.format(zeta=zeta) for zeta in ['D', 'T', 'Q', '5', '6']]
+double=['d-aug-cc-p{zeta}Z'.format(zeta=zeta) for zeta in ['D', 'T', 'Q', '5', '6']]
+triple=['t-aug-cc-p{zeta}Z'.format(zeta=zeta) for zeta in ['D', 'T', 'Q', '5', '6']]
+basis = single + double + triple
+
+mol=['H2', 'Ne', 'H2O', 'SO2']
+results = []
+for b in basis:
+    try:
+        results[b] = dr.get_polar_json(mol, xc, op, b)
+    except (FileNotFoundError, KeyError):
+        print(f"File not found for {b}")
+        continue
+
+print(results)
+
